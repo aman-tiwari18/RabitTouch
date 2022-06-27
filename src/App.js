54614167b0ba0ee8cf968a1216564f9968ff0bc5
Aman Tiwari <amantiwariald18@gmail.com>
2022-06-27 08:44:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import "./App.css";
+import Navabar from './Navabar';
+import Type from './type';
+
+
+function App() {
+  
+
+
+  return (
+    <div className="App">
+      <Navabar/>
+      <Type/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navabar.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Type from "./type";
+function Navabar(){
+    function refresh(){
+        window.location.reload(false);
+    }
+    
+    
+    
+    return (
+        <nav className = "nav">
+            
+            <button onClick = {refresh} className = " rbutton">Rabittype</button>
+            <ul>
+                <h3 className = "time">time</h3>
+                <span className = "gap">
+                <button className = "tbutton">30</button>
+                <button className = "tbutton">60</button>
+                </span>
+                
+            </ul>
+        </nav>
+    );
+}
+
+export default Navabar;
